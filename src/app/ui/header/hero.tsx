5642c62722a39154a30b0e7562a986165cda2067
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2024-02-14 00:09:37 -0300

    comillas 2

--- src/app/ui/header/hero.tsx
+++ src/app/ui/header/hero.tsx
@@ -14,12 +14,12 @@ export default function Hero() {
       <div className="glass p-4 rounded-lg shadow-2xl">
         <h1 className="text-7xl font-bold ">Biunatu App</h1>
         <p className="py-6">
-          "Biunatu is a haven for nature enthusiasts and adventurers alike,
+          Biunatu is a haven for nature enthusiasts and adventurers alike,
           offering a platform to share captivating photos, inspiring stories,
-          and thrilling trekking experiences. Whether you're passionate about
+          and thrilling trekking experiences. Whether you are passionate about
           exploring the great outdoors or simply in awe of the beauty of nature,
           AmazingNature invites you to connect with like-minded individuals and
-          immerse yourself in the wonders of the natural world."
+          immerse yourself in the wonders of the natural world.
         </p>
         <Link href={"/login"} className="btn glass ">Get Started</Link>
         
